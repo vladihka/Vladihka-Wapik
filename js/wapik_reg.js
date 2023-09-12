bcbedaf9dec447fa22a18a6a1516e08253add526
vladihka <vladihka58@gmail.com>
2023-09-12 12:47:04 +0200

    Reworking the code to make it easier to read

--- js/wapik_reg.js
+++ js/wapik_reg.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	$(".login_reg").click(function(event) {
+	$(".login__reg").click(function(event) {
 		$(".login").css('display', 'none');
 		$(".register").css('display','block');
 	});
-	$(".register_log").click(function(event) {
+	$(".register__log").click(function(event) {
 		$(".login").css('display', 'block');
 		$(".register").css('display','none');
 	});
